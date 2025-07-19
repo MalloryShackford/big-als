3db9fd3bdece68705da61a2ccbd53f82a3bb03de
Mallory Shackford <93612680+MalloryShackford@users.noreply.github.com>
2025-07-19 09:00:27 -0400

    updated map on contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -30,7 +30,7 @@ const Contact = () => {
       </p>
       <div className="map-container">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2939.7651962557466!2d-71.3032432!3d42.5390423!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e398a45bb003e5%3A0x245dab357b868296!2s240%20Nashua%20Rd%2C%20North%20Billerica%2C%20MA%2001862!5e0!3m2!1sen!2sus!4v1741873529767!5m2!1sen!2sus"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d94072.48628018376!2d-71.4350791375!3d42.5390423!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e39978d0b4a9b7%3A0xa760dbbfd6e8d91!2sBig%20Al&#39;s%20Pizza!5e0!3m2!1sen!2sus!4v1752929860078!5m2!1sen!2sus"
           width="400"
           height="300"
           style={{ border: '0' }}
