65e23ab5533dd7976f521f9a8f69c548aff32bc8
Mallory Shackford <93612680+MalloryShackford@users.noreply.github.com>
2025-08-10 08:00:10 -0400

    add vercel trailingSlash config, set up canonical url and helmet provider

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { HelmetProvider } from 'react-helmet-async';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider> 
     </BrowserRouter>
   </React.StrictMode>
 );
